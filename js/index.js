893c7f9efd3e50b922f62af2e30e988cb34f4ceb
bajohn <bjohn454@gmail.com>
2017-01-31 21:24:01 -0500

    page transitions added; apache config files added to repo; start/stop/restart scripts added (pid.out ignored by git)

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+$(document).ready( function() {
+	$("#join-button").click(function()
+	{
+		console.log("hi")
+	})
+})
--- js/mover.js
+++ js/mover.js
@@ -1,1 +1,50 @@
-console.log("hello")
+
+
+function pageChanger(html)
+{
+	
+		$("#page-container").animate({
+			"opacity": 0.25
+			}, 200, function() {
+				$("#page-container").html(html)
+				$("#page-container").animate({
+					"opacity": 1.0
+				}, 200, function()
+				{
+			
+				})
+			})
+		
+		
+}
+function loadPageAjax(url)
+{
+	$.ajax({
+		url: url,
+	}).done(function(resp) {
+		pageChanger(resp);
+	});
+}
+
+var initialLoad = false;
+
+$(document).ready( function() {
+	if(!initialLoad)
+{
+	loadPageAjax('/index-body');
+	initialLoad = true;
+	console.log(initialLoad)
+}
+	//link routing!
+	console.log(initialLoad)
+
+	
+	
+	$("#login-button").click(function() {
+		loadPageAjax('/login');
+	});
+	$("#header-logo").click(function() {
+		loadPageAjax('/index-body');
+		
+	})
+})
